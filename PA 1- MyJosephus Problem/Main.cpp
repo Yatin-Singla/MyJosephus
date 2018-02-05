66a6fab9e34b419a279a1c8d0f2c2ff9dc10ae06
Yatin Singla <yatinsingla2@gmail.com>
2018-02-05 07:39:03 -0800

    Starter code for main

---
+++ PA 1- MyJosephus Problem/Main.cpp
@@ -0,0 +1,15 @@
+/*
+* Name: Yatin Singla	
+* Programming Assignment: 1
+* Date: 3rd February 2018
+* Description: My Josephus Problem	
+* 
+*/
+
+#include "Person.h"
+
+int main(void)
+{
+
+	return 0;
+}
